19b09e86f0a9c297999c74984524b01eec30dc63
Joachim Danmayr <joachim@danmayr.org>
2025-01-26 07:44:37 +0000

    chore: remove with_mkldnn

--- recipes/libtorch/all/conanfile.py
+++ recipes/libtorch/all/conanfile.py
@@ -203,7 +203,6 @@ class LibtorchConan(ConanFile):
             del self.options.with_nnpack
         if is_apple_os(self):
             # Apple does not support NN packages
-            del self.options.with_mkldnn
             del self.options.with_qnnpack
         # J.D There is an option to enable or disable it, this should not be overidden
         #self.options.with_itt = self.settings.arch in ["x86", "x86_64"]
