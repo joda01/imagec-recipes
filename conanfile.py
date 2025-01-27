637d39ea83ada483faa167900fb813255e3c017a
Joachim Danmayr <joachim@danmayr.org>
2025-01-27 04:52:12 +0000

    chore: Fixed another dependency

--- conanfile.py
+++ conanfile.py
@@ -26,6 +26,7 @@ class ImageC(ConanFile):
         self.requires("sleef/3.6.1")
         self.requires("libtorch/2.4.0")
         self.requires("tensorflow-lite/2.15.0")
+        self.requires("xnnpack/cci.20240229", override = True)
         self.requires("protobuf/3.21.12", override=True)
         self.requires("flatbuffers/24.3.25", override=True)
         #self.requires("xkbcommon/1.6.0", override=True)
