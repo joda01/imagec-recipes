e4fe6a55c90466da460c21c846f4c5d16568a659
Joachim Danmayr <joachim@danmayr.org>
2025-01-27 18:59:01 +0000

    chore: Fixed patch

--- conanfile.py
+++ conanfile.py
@@ -23,7 +23,7 @@ class ImageC(ConanFile):
         #self.requires("libxlsxwriter/1.1.8")
         #self.requires("duckdb/1.1.0")
         #self.requires("onnx/1.17.0")
-        self.requires("sleef/3.6.1")
+        #self.requires("sleef/3.6.1")
         self.requires("libtorch/2.4.0")
         #self.requires("tensorflow-lite/2.15.0")
         #self.requires("xnnpack/cci.20240229", override = True)
