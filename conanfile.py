200d9abdc817bb04031d26fa3d0b02767a3551be
Joachim Danmayr <joachim@danmayr.org>
2025-02-03 17:04:49 +0000

    chore: fpgemm fix

--- conanfile.py
+++ conanfile.py
@@ -14,26 +14,28 @@ class ImageC(ConanFile):
     exports_sources = "src/*"
     
     def requirements(self):
-        self.requires("qt/6.7.1")
-        self.requires("opencv/4.10.0")
-        self.requires("catch2/3.7.0")
-        self.requires("pugixml/1.14")
-        self.requires("nlohmann_json/3.11.3")
-        self.requires("libxlsxwriter/1.1.8")
-        self.requires("duckdb/1.1.0")
-        self.requires("onnx/1.17.0", force=True)
-        self.requires("rapidyaml/0.7.1")
-        self.requires("tensorflow-lite/2.15.0")
-        self.requires("onnxruntime/1.18.1")
-        self.requires("libtorch/2.4.0") # Not supported for MinGW    
-        self.requires("flatbuffers/23.5.26", override=True)
-        self.requires("protobuf/3.21.12", override=True)
-        self.requires("xkbcommon/1.6.0", override=True)
-        self.requires("libpq/15.5", override=True)
-        self.requires("abseil/20240116.1", override=True)
-        self.requires("libbacktrace/cci.20240730", override = True)
-        self.requires("xnnpack/cci.20240229", override = True)
-        self.requires("boost/1.86.0", override = True)
+        self.requires("fbgemm/0.8.0")
+        
+        #self.requires("qt/6.7.1")
+        #self.requires("opencv/4.10.0")
+        #self.requires("catch2/3.7.0")
+        #self.requires("pugixml/1.14")
+        #self.requires("nlohmann_json/3.11.3")
+        #self.requires("libxlsxwriter/1.1.8")
+        #self.requires("duckdb/1.1.0")
+        #self.requires("onnx/1.17.0", force=True)
+        #self.requires("rapidyaml/0.7.1")
+        #self.requires("tensorflow-lite/2.15.0")
+        #self.requires("onnxruntime/1.18.1")
+        #self.requires("libtorch/2.4.0") # Not supported for MinGW    
+        #self.requires("flatbuffers/23.5.26", override=True)
+        #self.requires("protobuf/3.21.12", override=True)
+        #self.requires("xkbcommon/1.6.0", override=True)
+        #self.requires("libpq/15.5", override=True)
+        #self.requires("abseil/20240116.1", override=True)
+        #self.requires("libbacktrace/cci.20240730", override = True)
+        #self.requires("xnnpack/cci.20240229", override = True)
+        #self.requires("boost/1.86.0", override = True)
 
 
 
