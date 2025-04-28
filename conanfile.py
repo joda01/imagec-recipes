9312fe128cc06dc0891b986978721e859699420e
Joachim Danmayr <joachim@danmayr.org>
2025-04-28 04:12:41 +0000

    chore: Force httplib 19

--- conanfile.py
+++ conanfile.py
@@ -21,7 +21,7 @@ class ImageC(ConanFile):
         self.requires("nlohmann_json/3.11.3")
         self.requires("libxlsxwriter/1.1.8")
         self.requires("duckdb/1.1.3")
-        self.requires("cpp-httplib/0.19.0")
+        self.requires("cpp-httplib/0.19.0", force=True)
         self.requires("onnx/1.17.0", force=True)
         self.requires("rapidyaml/0.7.1")
         self.requires("tensorflow-lite/2.15.0")
