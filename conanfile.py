728191737de406a141b7eecca41388c554e04627
Joachim Danmayr <joachim@danmayr.org>
2025-10-12 17:29:01 +0000

    feat: Added cereal

--- conanfile.py
+++ conanfile.py
@@ -30,6 +30,7 @@ class ImageC(ConanFile):
         self.requires("cli11/2.5.0")
         self.requires("tensorflow-lite/2.15.0")
         self.requires("onnxruntime/1.18.1")
+        self.requires("cereal/1.3.2")
         self.requires("libtorch/2.4.0") # Not supported for MinGW    
         self.requires("matplotplusplus/1.2.2")
         self.requires("flatbuffers/23.5.26", override=True)
